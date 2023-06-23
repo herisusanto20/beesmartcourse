af550b8955eebf46f9e3e8d67bd79f30ad0419de
herisusanto20 <herilee2004@gmail.com>
2023-06-23 07:05:37 +0700

    hem

--- anah.php
+++ anah.php
@@ -41,26 +41,26 @@
         <!-- Navbar End -->
 
         
-            <section id="about" class="about">
+            <section id="about1" class="about1">
             <h2>Bee Smart Course itu?</h2>
 
-            <div class="row">
-            <div class="boxss">
-                <div class="about-img">
+            <div class="row1">
+            <div class="boxss1">
+                <div class="about-img11">
                     <div class="img1">
                     <img src="img/anah.jpg" alt="anah">
                     </div>
                     
                 </div>
-                <div class="content">
+                <div class="content1">
                 
                     <h3>Uswatun Chasanah S. Pd</h3>
                     <p>"Hallo Sobat BEE SMART, saya Uswatun Chasanah"</p>
                     <section>
-                        <div class="sections">
-                           <div class="containers">
-                                   <div class="boxs">
-                                     <div class="img2">
+                        <div class="sections1">
+                           <div class="containers1">
+                                   <div class="boxs1">
+                                     <div class="img21">
                                      <img src="img/beesmart1.png" alt="beesmart1">
                                             <img src="img/beesmart.png" alt="beesmart">
                                             <img src="img/beesmart1.png" alt="beesmart1">
